e1926e3c2c8fdf2fd3d8162ec10a40ac08df6f18
Mohammed98112 <seaomn27@gmail.com>
2026-06-24 10:08:01 +0400

    Added Pilot class inside Model

---
+++ Flight Management System/Models/pilot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flight_Management_System.Models
+{
+    public class Pilot
+    {
+        public int PilotId { get; set; }          // System Generated
+        public string PilotName { get; set; }     // User Input
+        public string PilotPhone { get; set; }    // User Input
+        public string LicenseNumber { get; set; } // User Input
+        public int FlightHours { get; set; }      // User Input 
+        public bool IsAvailable { get; set; }     // Default Value 
+    }
+}
