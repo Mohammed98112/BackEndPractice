e6fe53d6f8085051855237889dbeeb7901b63844
Mohammed98112 <seaomn27@gmail.com>
2026-06-24 11:11:01 +0400

    Added Lists inside FlightContext class

--- Flight Management System/FlightContext.cs
+++ Flight Management System/FlightContext.cs
@@ -1,10 +1,18 @@
-﻿using System;
+﻿using Flight_Management_System.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Flight_Management_System
 {
-    internal class FlightContext
-    {
-    }
+   
+        public class FlightContext
+        {
+            public List<Passenger> Passengers { get; set; } = new List<Passenger>();
+            public List<Pilot> Pilots { get; set; } = new List<Pilot>();
+            public List<Aircraft> Aircrafts { get; set; } = new List<Aircraft>();
+            public List<Flight> Flights { get; set; } = new List<Flight>();
+            public List<Booking> Bookings { get; set; } = new List<Booking>();
+        }
+    
 }
