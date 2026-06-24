848256c5bb1dfc5e05a961eb343b025a7d0a8df4
Mohammed98112 <seaomn27@gmail.com>
2026-06-24 10:45:37 +0400

    Added Flight Entities

--- Flight Management System/Models/Flight.cs
+++ Flight Management System/Models/Flight.cs
@@ -6,6 +6,16 @@ namespace Flight_Management_System.Models
 {
     public class Flight
     {
- 
+        public int FlightId { get; set; }        // System Generated
+        public string FlightCode { get; set; }   // System Generated
+        public int AircraftId { get; set; }      // From List
+        public int PilotId { get; set; }         // From List
+        public string Origin { get; set; }       // User Input
+        public string Destination { get; set; }  // User Input
+        public string DepartureDate { get; set; }// User Input
+        public string DepartureTime { get; set; }// User Input
+        public decimal TicketPrice { get; set; } // User Input
+        public int AvailableSeats { get; set; }  // System Calculated 
+        public string Status { get; set; }       // Default Value 
     }
 }
