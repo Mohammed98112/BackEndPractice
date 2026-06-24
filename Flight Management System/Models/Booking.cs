13b25cd85e9eb5cd6c95e48f6c8e3daecdc4b85f
Mohammed98112 <seaomn27@gmail.com>
2026-06-24 10:59:32 +0400

    ADDED Attributes inside Booking Class

--- Flight Management System/Models/Booking.cs
+++ Flight Management System/Models/Booking.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Flight_Management_System.Models
 {
-    internal class Booking
+    public class Booking
     {
+        public int BookingId { get; set; }       // System Generated
+        public int PassengerId { get; set; }     // From List
+        public int FlightId { get; set; }        // From List
+        public string SeatNumber { get; set; }   // System Generated 
+        public string BookingDate { get; set; }  // System Generated (today)
+        public decimal TotalPrice { get; set; }  // Calculated 
+        public string Status { get; set; }       // Default Value 
     }
 }
