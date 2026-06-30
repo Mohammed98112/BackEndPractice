b24332966d0bd051b11913dc19d07bb8aca68b1a
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 17:20:46 +0400

    ended department annotations

--- UniversitySystem/Models/Department.cs
+++ UniversitySystem/Models/Department.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystem.Models
 {
     public class Department
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int departmentId { get; set; }         // system generated
-        public string departmentName { get; set; }      // user input
-        public string building { get; set; }              // user input 
+        [Required]
+        [StringLength(100)]
+        public string? departmentName { get; set; }      // user input
+        [StringLength(50)]
+        public string building { get; set; }              // user input
+        [Required]
+        [Range(0,double.MaxValue)]
         public decimal budget { get; set; }                 // user input
+        [ForeignKey("Instuctor")]
         public int? headInstructorId { get; set; }            // foreign key 
     }
 }
