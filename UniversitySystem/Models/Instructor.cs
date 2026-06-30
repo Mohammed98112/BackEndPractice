1255844df26842f5320051536dd15b4df5a50de7
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 15:23:03 +0400

    Instructor Attribues

--- UniversitySystem/Models/Instructor.cs
+++ UniversitySystem/Models/Instructor.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversitySystem.Models
 {
-    internal class Instructor
+    public class Instructor
     {
+        public int instructorId { get; set; }        
+        public string fullName { get; set; }          
+        public string email { get; set; }               
+        public string officeNumber { get; set; }         
+        public DateTime hireDate { get; set; }            
+        public decimal salary { get; set; }                 
+        public string academicTitle { get; set; }            
     }
 }
