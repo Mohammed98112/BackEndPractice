b2a49d5c90723da13fb5cb399d50569336b39c74
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 17:20:59 +0400

    ended Enrolment annotations

--- UniversitySystem/Models/Enrrolment.cs
+++ UniversitySystem/Models/Enrrolment.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
 namespace UniversitySystem.Models
 {
     public class Enrollment
     {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int enrollmentId { get; set; }          // system generated
+        [ForeignKey("Student")]
         public int studentId { get; set; }                // foreign key
+        [ForeignKey("Customer")]
         public int courseId { get; set; }                   // foreign key
+        [Required]
         public DateTime enrollmentDate { get; set; }           // system generated
-        public string finalGrade { get; set; }                   // user input 
-        public string status { get; set; }                         // default value "In Progress" | "Completed" | "Withdrawn"
+        [StringLength(2)]
+        public string? finalGrade { get; set; }                   // user input 
+        [Required]
+        [StringLength(20)]
+        public string status { get; set; } = "Progress";                      // default value "In Progress" | "Completed" | "Withdrawn"
     }
 }
