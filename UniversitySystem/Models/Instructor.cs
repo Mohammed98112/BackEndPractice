e5ce3d61f4bc327c7123e9eca4b869c90c89f637
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 15:23:58 +0400

    attributes comments

--- UniversitySystem/Models/Instructor.cs
+++ UniversitySystem/Models/Instructor.cs
@@ -6,12 +6,12 @@ namespace UniversitySystem.Models
 {
     public class Instructor
     {
-        public int instructorId { get; set; }        
-        public string fullName { get; set; }          
-        public string email { get; set; }               
-        public string officeNumber { get; set; }         
-        public DateTime hireDate { get; set; }            
-        public decimal salary { get; set; }                 
-        public string academicTitle { get; set; }            
+        public int instructorId { get; set; }        // system generated
+        public string fullName { get; set; }          // user input
+        public string email { get; set; }               // user input
+        public string officeNumber { get; set; }         // user input 
+        public DateTime hireDate { get; set; }            // user input
+        public decimal salary { get; set; }                 // user input
+        public string academicTitle { get; set; }            // user input (from list)
     }
 }
