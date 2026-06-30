f51a05a94bd60c4a003f235a7cab18c609f3a2b1
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 15:14:00 +0400

    created Course Class

---
+++ UniversitySystem/Models/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UniversitySystem.Models
+{
+    public class Course
+    {
+        public int courseId { get; set; }              // system generated
+        public string courseCode { get; set; }           // user input
+        public string courseTitle { get; set; }            // user input
+        public int creditHours { get; set; }                 // user input
+        public int departmentId { get; set; }                  // foreign key
+        public int instructorId { get; set; }                   // foreign key 
+        public string semesterOffered { get; set; }                // user input (from list)
+    }
+}
