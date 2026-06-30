9f79284d5291fb0ff1f02e88606c882326abeaa8
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 15:29:15 +0400

    attributes comments

--- UniversitySystem/Models/Department.cs
+++ UniversitySystem/Models/Department.cs
@@ -6,10 +6,10 @@ namespace UniversitySystem.Models
 {
     public class Department
     {
-        public int departmentId { get; set; }         
-        public string departmentName { get; set; }      
-        public string building { get; set; }              
-        public decimal budget { get; set; }                 
-        public int? headInstructorId { get; set; }            
+        public int departmentId { get; set; }         // system generated
+        public string departmentName { get; set; }      // user input
+        public string building { get; set; }              // user input 
+        public decimal budget { get; set; }                 // user input
+        public int? headInstructorId { get; set; }            // foreign key 
     }
 }
