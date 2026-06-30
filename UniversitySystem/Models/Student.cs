e86b4fe9abd627fa7cc5848af88513754973407a
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 15:17:51 +0400

    created student class attributes

--- UniversitySystem/Models/Student.cs
+++ UniversitySystem/Models/Student.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace UniversitySystem.Models
 {
-    internal class Student
+    public class Student
     {
+        public int studentId { get; set; }          
+        public string fullName { get; set; }         
+        public string email { get; set; }             
+        public string phoneNumber { get; set; }       
+        public DateTime dateOfBirth { get; set; }      
+        public int enrollmentYear { get; set; }         
+        public decimal gpa { get; set; }                 
     }
 }
