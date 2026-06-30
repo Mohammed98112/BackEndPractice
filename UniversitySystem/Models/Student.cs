3f472448edefecbb2550281fc0ff0f6cd089301d
Mohammed98112 <seaomn27@gmail.com>
2026-06-30 16:43:29 +0400

    Student updated annotations

--- UniversitySystem/Models/Student.cs
+++ UniversitySystem/Models/Student.cs
@@ -1,29 +1,30 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
-
+using System.Xml.Linq;
 namespace UniversitySystem.Models
 {
+    [Index(nameof(email), IsUnique = true)]
     public class Student
     {
-        [key]
-        [Required]
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int studentId { get; set; }          // system generated
-        [Required]
-        [MaxLength(100)]
+        [Required, MaxLength(100)]
         public string fullName { get; set; }         // user input
         [Required]
         [MaxLength(150)]
         public string email { get; set; }             // user input
         [MaxLength(20)]
-        public string phoneNumber { get; set; }       // user input 
+        public string? phoneNumber { get; set; }       // user input 
         [Required]
         public DateTime dateOfBirth { get; set; }      // user input
         [Required]
         [Range(2000,2030)]
         public int enrollmentYear { get; set; }         // user input
         [Range(0.0, 4.0)]
-        public decimal gpa { get; set; }                 // System calculated
+        public decimal gpa { get; set; } = 0.0m;               // System calculated
     }
 }
