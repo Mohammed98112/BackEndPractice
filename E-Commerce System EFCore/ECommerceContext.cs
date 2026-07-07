3c82eeb4fe6c4c4c001b37e7ebc062b65afb3684
Mohammed98112 <seaomn27@gmail.com>
2026-07-07 09:59:44 +0400

    DbSet<Product>

--- E-Commerce System EFCore/ECommerceContext.cs
+++ E-Commerce System EFCore/ECommerceContext.cs
@@ -11,6 +11,7 @@ namespace E_Commerce_System_ERD___Models_Copy
     {
         public DbSet<User> Users { get; set; }
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
       
     }
 }
