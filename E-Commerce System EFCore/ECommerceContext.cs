af3320b1220d7f40a4d9ffbef83b37038fffbfc9
Mohammed98112 <seaomn27@gmail.com>
2026-07-07 09:58:08 +0400

    created ECommerceContext class

---
+++ E-Commerce System EFCore/ECommerceContext.cs
@@ -0,0 +1,14 @@
+﻿using E_Commerce_System_EFCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace E_Commerce_System_ERD___Models_Copy
+{
+    public class ECommerceContext : DbContext
+    {
+        
+    }
+}
