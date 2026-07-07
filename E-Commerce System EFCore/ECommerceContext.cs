67798ede8a6f8bc7f0742819da917af14d1c6fb1
Mohammed98112 <seaomn27@gmail.com>
2026-07-07 10:00:24 +0400

    DbSet<OrderProduct>

--- E-Commerce System EFCore/ECommerceContext.cs
+++ E-Commerce System EFCore/ECommerceContext.cs
@@ -13,6 +13,7 @@ namespace E_Commerce_System_ERD___Models_Copy
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderProduct> OrderItems { get; set; }
       
     }
 }
