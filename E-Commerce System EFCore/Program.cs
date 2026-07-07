922140faefd81b22d9eae0624fa4f97431a18c74
Mohammed98112 <seaomn27@gmail.com>
2026-07-07 09:41:03 +0400

    copied old project to apply new task on it(EFCore), copied the whole project with models and classes

---
+++ E-Commerce System EFCore/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace E_Commerce_System_EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
